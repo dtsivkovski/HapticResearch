9106914270d5877b6e94a614331bc1f7595be555
Daniel Tsivkovski <dtsivkovs@gmail.com>
2023-11-15 22:21:33 -0800

    serial js works

--- USB Server/USB Integration/serial.js
+++ USB Server/USB Integration/serial.js
@@ -2,14 +2,14 @@ const {SerialPort} = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline')
 
 // Replace 'COM3' with the appropriate serial port name for your Arduino
-const port = new SerialPort({ path: 'COM9', baudRate: 9600 });
+const port = new SerialPort({ path: 'COM5', baudRate: 9600 });
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 port.on('open', () => {
   console.log('Serial port is open.');
   // Send data to the Arduino
-  port.write('Your message to the Arduino\n', (err) => {
+  port.write('000001000280044008201ff0000000000000\n', (err) => {
     if (err) {
       return console.log('Error: ', err.message);
     }
