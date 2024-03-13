c419a70535f25c4bb57ee81fd4f668cc589a1499
Daniel Tsivkovski <dtsivkovs@gmail.com>
2024-03-13 11:58:09 -0700

    shape game beginning

---
+++ docs/assets/serial.js
@@ -0,0 +1,50 @@
+let serialPort;
+const connectButton = document.getElementById('connectButton');
+
+// function to connect to serial port
+connectButton.addEventListener('click', async () => {
+    try {
+    serialPort = await navigator.serial.requestPort();
+    await serialPort.open({ baudRate: 9600 });
+    console.log('Serial port connected.');
+    // document.getElementById('connection-status').textContent = 'Status: Connected';
+    // document.getElementById('connection-status').className = 'text-success';
+    document.getElementById('connectButton').disabled = true;
+    document.getElementById('connectButton').textContent = 'Connected';
+    document.getElementById('connectButton').className = 'btn btn-rounded btn-success';
+    // delay 2s
+    setTimeout(() => {
+        sendData("1111111110000001100000011000010110101001100100011000000111111111"); // symbol to show connection success
+    }, 2000);
+    } catch (error) {
+    console.error('Error connecting to serial port:', error);
+    // document.getElementById('connection-status').textContent = 'Connection failed. Please try again.';
+    // document.getElementById('connection-status').className = 'text-danger';
+    alert('Error connecting to serial port. Please try again.');
+    }
+});
+
+// function to send data
+async function sendData(data) {
+    if (!serialPort || !serialPort.writable) {
+    console.error('Serial port not connected or not writable.');
+    if(!serialPort) {
+        // alert('Please connect to the serial port first.');
+        return;
+    }
+    else if(!serialPort.writable) {
+        // alert('Serial port is not writable. Please reconnect.');
+    }
+    return;
+    }
+
+    // encodes the data being sent to the arduino port
+    const encoder = new TextEncoder();
+    const dataArray = encoder.encode(data);
+
+    // writing to the serial port of the arduino
+    const writer = serialPort.writable.getWriter();
+    await writer.write(dataArray);
+    writer.releaseLock();
+    console.log('Data sent to Arduino:', data);
+}
---
+++ docs/assets/shapegame.js
@@ -0,0 +1,197 @@
+
+
+var pointTotal = 0;
+var streak = 0;
+
+var correctAnswer = ""; // used to save the text of the correct answer
+
+// add tts to necessary focus elements
+document.querySelector('#sgButton').addEventListener('focus', function() {
+    speak(this.value);
+});
+
+const shapeDict = {
+    // dummy values temporarily
+    "circle": "clclclclclclclc",
+    "square": "sqsqsqsqsqsqsqsqsqs",
+    "triangle": "trtrtrtrttrrtrt",
+    "rectangle": "rcrcrcrcrcrcrc",
+    "plus-cross": "++++++++++++++++",
+    "x-cross": "xxxxxxxxxxxxxxxx",
+    "heart": "3333333333333333"
+}
+
+
+function playGame() {
+
+    document.querySelector('#gamediv').style = "display: block !important";
+    document.querySelector('#sgButton').innerHTML = "Skip Question";
+    document.querySelector('#sgButton').value = "Press space to skip this question";
+    document.activeElement.blur(); // remove focus from the current element
+    currentFocus = 0; // set focus to the first answer radio
+    speak("What is this shape?");
+    // generate shape number
+    var shapeNum = -1;
+
+    const shapeWordBank = Object.keys(shapeDict);
+
+    // make sure denominator and numerator aren't zero, numerator in the single digits
+    while (shapeNum == -1) {
+        shapeNum = Math.floor(Math.random() * shapeWordBank.length);
+    }
+
+    // calculate the answer and build the numstring
+    document.querySelector('#gamequestion').textContent = "What is this shape?";
+    var correctAnswer;
+
+    // save the correct answer
+    correctAnswer = shapeWordBank[shapeNum];
+
+    /* 
+    Randomize the answers for the numerator
+    */
+    var answerBank = [];
+    answerBank.push(correctAnswer);
+    while (answerBank.length < 4) {
+        var randNum = Math.floor((Math.random() * shapeWordBank.length)); // *9 + 1 in order to avoid zero/error
+        if (!answerBank.includes(shapeWordBank[randNum])) {
+            answerBank.push(shapeWordBank[randNum]);
+        }
+    }
+
+    // shuffle the array
+    answerBank = shuffle(answerBank);
+
+    // create buttons for each answer and add to the divs
+    var answerButtonDiv = document.querySelector('#answerButtons');
+
+    answerButtonDiv.innerHTML = "";
+
+    for (i = 0; i < 4; i++) {
+    // radios for the form types 
+    /* 
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+        <label class="form-check-label" for="exampleRadios1">
+            Default radio
+        </label>
+        </div>
+    */
+    // numerator answer radios
+    var shapeForm = document.createElement('div');
+    shapeForm.className = "form-check";
+    // creating the radio input
+    var shapeRadio = document.createElement('input');
+    shapeRadio.className = "form-check-input";
+    shapeRadio.type = "radio";
+    shapeRadio.name = "numAnswerRadios";
+    shapeRadio.id = "numAnswer_" + i;
+    shapeRadio.value = answerBank[i];
+    var numLabel = document.createElement('label');
+    // creating the label for hte input
+    numLabel.className = "form-check-label";
+    numLabel.htmlFor = answerBank[i];
+    numLabel.textContent = answerBank[i];
+
+    // add event listener for focus
+    shapeRadio.addEventListener('focus', function() {
+        speak(this.value);
+    });
+    shapeRadio.addEventListener('click', function() {
+        speak("You selected " + this.value);
+    });
+
+    // append the radio and label to the div
+    shapeForm.appendChild(shapeRadio);
+    shapeForm.appendChild(numLabel);
+    answerButtonDiv.appendChild(shapeForm);
+
+    }
+
+    console.log("Answer " + i + ": " + answerBank[i]);
+    sendData(shapeDict[answerBank[i]]);
+
+}
+
+// check for enter key press when submitting answer
+
+document.querySelector('#checkAnswerButton').addEventListener('click', checkAnswer);
+document.querySelector('#checkAnswerButton').addEventListener('focus', function() {
+    // tries to speak selected answers
+    try {
+    speak('You selected ' + document.querySelector('input[name="numAnswerRadios"]:checked').value 
+                        + " " 
+                        + document.querySelector('input[name="denomAnswerRadios"]:checked').value
+                        + ". Press space to submit.");
+    }
+    catch (error) {
+    speak('Please select an answer before submitting.'); // if no answer is selected
+    }
+});
+
+function checkAnswer() {
+    // if both answers are not selected, do not check
+    if (!document.querySelector('input[name="numAnswerRadios"]:checked') || !document.querySelector('input[name="denomAnswerRadios"]:checked')) {
+    speak('Please select an answer before submitting.');
+    return;
+    }
+
+    document.querySelector('#checkAnswerButton').classList.add('disabled');
+    document.querySelector('#checkAnswerButton').removeEventListener('click', checkAnswer);
+    // get user answer, correct answer is from the hidden div
+    var userAnswer = document.querySelector('input[name="numAnswerRadios"]:checked').value 
+                    + " " 
+                    + document.querySelector('input[name="denomAnswerRadios"]:checked').value;
+    // get divs for displaying scores and answer correct/incorrect
+    const answerStatus = document.querySelector('#answerStatus');
+    const pointTotalDiv = document.querySelector('#pointTotal');
+    const streakDiv = document.querySelector('#streak');
+    if (userAnswer == correctAnswer) {
+    // correct answer
+    answerStatus.textContent = 'Correct!';
+    answerStatus.className = 'text-success';
+    pointTotalDiv.textContent = 'Points: ' + ++pointTotal;
+    streakDiv.textContent = 'Streak: ' + ++streak;
+    speak("Correct! " + "You have " + pointTotal + " points.");
+    if (streak % 5 == 0) {
+        speak("Wow! " + streak + " in a row! Keep it up!");
+    }
+    } else {
+    // incorrect answer - gives feedback for the correct answer
+    answerStatus.textContent = 'Incorrect. The correct answer is ' + correctAnswer;
+    answerStatus.className = 'text-danger';
+    streak = 0;
+    streakDiv.textContent = 'Streak: ' + streak;
+    }
+    
+    // reset after 4 seconds
+    setTimeout( () => {
+    answerStatus.textContent = '';
+    document.querySelector('#checkAnswerButton').classList.remove('disabled');
+    document.querySelector('#checkAnswerButton').addEventListener('click', checkAnswer);
+    }, 4000);
+
+    // wait 4 seconds, then play again
+    setTimeout(playGame, 4000);
+}
+
+/* 
+    Begin code referenced from GeeksForGeeks:
+    https://www.geeksforgeeks.org/how-to-shuffle-an-array-using-javascript/
+*/
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) { 
+    
+        // Generate random number 
+        var j = Math.floor(Math.random() * (i + 1));
+                    
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+        
+    return array;
+}
+/*
+    End of referenced code
+*/
