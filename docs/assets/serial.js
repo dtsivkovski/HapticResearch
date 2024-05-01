c505dad243a814a8d353d256abd2d0d4755a41f3
dtsivkovski <dtsivkovs@gmail.com>
2024-05-01 11:54:41 -0700

    serial.js indentation

--- docs/assets/serial.js
+++ docs/assets/serial.js
@@ -4,27 +4,25 @@ const connectButton = document.getElementById('connectButton');
 // function to connect to serial port
 connectButton.addEventListener('click', async () => {
     try {
-    let filters = [
-        { usbVendorId: 0x2341, usbProductId: 0x0043 },
-        { usbVendorId: 0x2341, usbProductId: 0x0001 }
-    ];
-    serialPort = await navigator.serial.requestPort({filters});
-    await serialPort.open({ baudRate: 9600 });
-    console.log('Serial port connected.');
-    // document.getElementById('connection-status').textContent = 'Status: Connected';
-    // document.getElementById('connection-status').className = 'text-success';
-    document.getElementById('connectButton').disabled = true;
-    document.getElementById('connectButton').textContent = 'Connected';
-    document.getElementById('connectButton').className = 'btn btn-rounded btn-success';
-    // delay 2s
-    setTimeout(() => {
-        sendData("1111111110000001100000011000010110101001100100011000000111111111"); // symbol to show connection success
-    }, 2000);
+        let filters = [
+            { usbVendorId: 0x2341, usbProductId: 0x0043 },
+            { usbVendorId: 0x2341, usbProductId: 0x0001 }
+        ];
+        serialPort = await navigator.serial.requestPort({filters});
+        await serialPort.open({ baudRate: 9600 });
+        console.log('Serial port connected.');
+        // document.getElementById('connection-status').textContent = 'Status: Connected';
+        // document.getElementById('connection-status').className = 'text-success';
+        document.getElementById('connectButton').disabled = true;
+        document.getElementById('connectButton').textContent = 'Connected';
+        document.getElementById('connectButton').className = 'btn btn-rounded btn-success';
+        // delay 2s
+        setTimeout(() => {
+            sendData("1111111110000001100000011000010110101001100100011000000111111111"); // symbol to show connection success
+        }, 2000);
     } catch (error) {
-    console.error('Error connecting to serial port:', error);
-    // document.getElementById('connection-status').textContent = 'Connection failed. Please try again.';
-    // document.getElementById('connection-status').className = 'text-danger';
-    alert('Error connecting to serial port. Please try again.');
+        console.error('Error connecting to serial port:', error);
+        alert('Error connecting to serial port. Please try again.');
     }
 });
 
