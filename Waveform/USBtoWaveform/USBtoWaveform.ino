15b3c39b83827341a7ffee98ef9e70105d808aa0
dtsivkovski <dtsivkovs@gmail.com>
2024-09-08 19:53:00 -0700

    TCA and PORt functions started

--- Waveform/USBtoWaveform/USBtoWaveform.ino
+++ Waveform/USBtoWaveform/USBtoWaveform.ino
@@ -26,7 +26,6 @@ const int waveformPositions[6][6] = {
   {4,4,5,5,6,6},
   {4,4,5,5,6,6}
 };
-
 */
 
 void setup() {
@@ -71,18 +70,56 @@ void binaryToWaveForm(String bString) {
     return;
   }
 
-  for (int row = 0; row < nrows; row++) {
-    for (int col = 0; col < ncols; col++) {
-        char bval = bString[(row * ncols) + col];
+  // iterating through entire string
+  for (int i = 0; i < bString.length(); i++) {
+    // values
+    int row = i / 8;
+    int col = i % 8;
 
-        if (bval == '1') {
-          // send to waveform using position and value at that position
-          // sendWaveform(waveformPositions[row][col], waveformValues[row][col]);
-        } else {
-        // do not draw (turn off waveform at that position)
+    // get character values
+    char bval = bString[(row * 8) + col + 1]; // adding 1 because first char is the ~ char
+
+    // set TCA and PORT and if it's a gap TCA (TCAs 7-8)
+    int TCA = 0;
+    int port = 0;
+    bool gapTCA = false;
+      // top row of TCAs - 0-2,6
+      if (row < 4) {
+        
+        if (col == 2 || col == 5) {
+          TCA = 6;
+          gapTCA = true;
+        }
+        else if (col < 2) {
+          TCA = 0;
+        }
+        else if (col < 5) {
+          TCA = 1;
+        }
+        else {
+          TCA = 2;
+        }
+        
       }
+      else { // bottom row of TCAs - 3-5,7
+
+        if (col == 2 || col == 5) {
+          TCA = 7;
+          gapTCA = true;
+        }
+        else if (col < 2) {
+          TCA = 3;
+        }
+        else if (col < 5) {
+          TCA = 4;
+        }
+        else {
+          TCA = 5;
+        }
+        
       }
-    }
+      
+  }
 }
 
 void checkString(String data) {
